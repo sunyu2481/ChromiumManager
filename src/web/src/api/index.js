0c487ad64943616d448b7dd53ead86a04921e83d
sunyu <sunyu2481@foxmail.com>
2026-08-09 22:20:08 +0800

    [fix] 补回误删的 deleteProxy API 函数

--- src/web/src/api/index.js
+++ src/web/src/api/index.js
@@ -75,6 +75,11 @@ export const updateProxy = (data) => {
   return request.post(`/update_proxy`, data)
 }
 
+export const deleteProxy = (id) => {
+  return request.post(`/delete_proxy`, { id })
+}
+
+// Agent APIs
 export const getAgentConfig = () => {
   return request.get('/get_agent_config')
 }
